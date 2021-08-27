e522762fbe1d648f54d3c5c3c85fe50711e5cd22
leonid <yaaln17@gmail.com>
2021-08-27 13:53:01 +0600

    Some changes in HomePage classes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -19,7 +19,7 @@ export const HomePage = () => {
         <>
         <div className="row padding_top_5">
         <NavLink to="/create">
-            <div className="col s3">
+            <div className="col s3 hoverable">
                 <div className="home-icons-block">
                         <i className="large material-icons">live_help</i>
                         <p>Создать заявку</p>
@@ -27,7 +27,7 @@ export const HomePage = () => {
             </div>
         </NavLink>
         <NavLink to="/">
-            <div className="col s3">
+            <div className="col s3 hoverable">
                 <div className="home-icons-block">
                         <i className="large material-icons">contact_phone</i>
                         <p>Справочник ОЦК</p>
@@ -35,7 +35,7 @@ export const HomePage = () => {
             </div>
         </NavLink>
         <NavLink to="/">
-            <div className="col s3">
+            <div className="col s3 hoverable">
                 <div className="home-icons-block">
                         <i className="large material-icons">library_books</i>
                         <p>Документы</p>
@@ -43,7 +43,7 @@ export const HomePage = () => {
             </div>
         </NavLink>
         <NavLink to="/">
-            <div className="col s3">
+            <div className="col s3 hoverable">
                 <div className="home-icons-block">
                         <i className="large material-icons">question_answer</i>
                         <p>Вопрос-Ответ</p>
